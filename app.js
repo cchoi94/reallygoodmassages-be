e0c75fd91eb8ec11b613edad0f154c4d8233f5c2
Cody Choi <cchoi_2294@hotmail.com>
2020-10-24 17:32:57 -0400

    added cors, errorResponse, error handler, and configured appjs

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 import express from 'express';
 import bodyParser from 'body-parser';
-// import session from 'express-session';
 import errorHandler from './middleware/error';
 // import userRoutes from './routes/users';
 
 const app = express();
-const PORT = 4200;
+const PORT = process.env.PORT || 4200;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
 app.use('/api/v1/users', userRoutes);
-// app.use('/api/v1/stripe', stripeRoutes);
 
 app.use(errorHandler);
 
